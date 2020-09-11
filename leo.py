4e1862da74ef8798d97104db5fb2d18146496e0d
jllanos <llanosmirandajuan@gmail.com>
2020-09-11 03:30:15 -0500

    Removing libraries

--- leo.py
+++ leo.py
@@ -1,13 +1,2 @@
-import numpy as np
-
-def media(_number1, _number2):
-
-    media =( _number1 + _number2 )/2
-    print(media)
-def mediana():
-    print("this function should calculate the mediana")
-
-def moda():
-    print("moda")
 
 print("Este es el script python de leo")
